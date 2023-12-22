facc67adc7defe4a93133526e1d8c8d16763b7c2
ebubekirakin <ebubekirakin_13@hotmail.com>
2023-12-22 13:06:54 +0300

    Create pozitifnegatif.java

---
+++ girilen 10 sayının kaçı pozitif kaçı negatif/pozitifnegatif.java
@@ -0,0 +1,50 @@
+package ders1;
+
+import java.util.Scanner;
+
+//girilen 10 sayının kaçı pozitif kaçı negatif?
+
+public class pozitifnegatif {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+int pozitif=0;
+int negatif=0;
+int sifir=0;
+
+for (int i=0;i<5;i++) {		
+ 
+	System.out.print("Bir sayı giriniz: ");
+
+int sayi=sc.nextInt();
+if (sayi>0) {
+	
+pozitif=pozitif+1;
+	
+}
+
+else if (sayi<0) {
+	
+	
+	negatif=negatif+1;
+	
+}
+
+else {
+	
+	sifir=sifir+1;
+}
+
+
+}
+		
+		
+		System.out.println("Pozitif sayı adedi: "+pozitif);
+		
+		System.out.println("Negatif sayı adedi: "+negatif);
+		
+		System.out.println("Girilen sıfır adedi: "+sifir);
+	}
+
+}
