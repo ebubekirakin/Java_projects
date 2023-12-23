a9a5c15f47278f95e518fc87ab67dd398727e4ed
ebubekirakin <ebubekirakin_13@hotmail.com>
2023-12-23 22:27:52 +0300

    Create yaşhesaplama.java

---
+++ Yaş hesaplama/yaşhesaplama.java
@@ -0,0 +1,33 @@
+package ders1;
+
+import java.util.Scanner;
+
+public class yaşhesaplama {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Doğum Yılınızı Giriniz: ");
+		
+		int yıl = sc.nextInt();
+		
+		int yas = 2023-yıl;
+		
+		System.out.println("Yaşınız: "+yas);
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+	}
+
+}
